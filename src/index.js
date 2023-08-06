394fce61a235987369578f466736e6fed7ed88a8
Sayan Nurmuhambet <s.deaiko@gmail.com>
2023-08-06 16:22:31 +0600

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = '';
+    let strNum = String(n);
+    for (let index = strNum.length - 1; index >= 0; index--) {
+        if (strNum[index] != '-') {
+            res += strNum[index];
+        }
+    }
+    return Number(res);
 }
